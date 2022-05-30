fcc02599f9c07fc205a9f5f89f1414e9e838051a
molwhy <molw_smartcloud@hotmail.com>
2022-05-30 11:33:31 +0800

    添加module

---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain/CommonDomainModule.cs
@@ -0,0 +1,11 @@
+﻿using SmartCloud.Common.Domain.Shared;
+using Volo.Abp.Modularity;
+
+namespace SmartCloud.Common.Domain
+{
+    [DependsOn(typeof(CommonDomainSharedModule))]
+    public class CommonDomainModule : AbpModule
+    {
+
+    }
+}
