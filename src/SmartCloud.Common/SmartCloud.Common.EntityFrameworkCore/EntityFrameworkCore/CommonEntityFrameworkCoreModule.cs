ee9519ea36f434e31767c554f2fc11f65605cf30
molwhy <molw_smartcloud@hotmail.com>
2022-05-30 11:49:22 +0800

    新增数据库访问层类库项目，并增加module功能，引用领域层项目

---
+++ src/SmartCloud.Common/SmartCloud.Common.EntityFrameworkCore/EntityFrameworkCore/CommonEntityFrameworkCoreModule.cs
@@ -0,0 +1,17 @@
+﻿using SmartCloud.Common.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Modularity;
+
+namespace SmartCloud.Common.EntityFrameworkCore.EntityFrameworkCore
+{
+    [DependsOn(
+        typeof(CommonDomainModule)
+    )]
+    public class CommonEntityFrameworkCoreModule : AbpModule
+    {
+    }
+}
