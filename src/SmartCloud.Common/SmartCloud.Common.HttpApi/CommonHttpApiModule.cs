319bcd4841208602f4346af88bcb2bc13deabded
weijhy <weij_smartcloud@hotmail.com>
2022-05-30 19:23:59 +0800

    新增HttpApi类库项目

---
+++ src/SmartCloud.Common/SmartCloud.Common.HttpApi/CommonHttpApiModule.cs
@@ -0,0 +1,12 @@
+﻿using Volo.Abp.Modularity;
+using SmartCloud.Common.Application.Contracts;
+namespace SmartCloud.Common.HttpApi
+{
+    [DependsOn(
+        typeof(CommonApplicationContractsModule)
+    )]
+    public class CommonHttpApiModule : AbpModule
+    {
+
+    }
+}
---
+++ src/SmartCloud.Common/SmartCloud.Common.HttpApi/Controllers/CommonController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace SmartCloud.Common.HttpApi.Controllers
+{
+    public abstract class CommonController : AbpController
+    {
+    }
+}
