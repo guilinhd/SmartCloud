2cd61597433de93b6333b8ab278d4a1654151c43
molwhy <molw_smartcloud@hotmail.com>
2022-05-30 18:16:13 +0800

    简化命名空间名称

--- src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/Datas/IDataAppService.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/Datas/IDataAppService.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Services;
 
-namespace SmartCloud.Common.Application.Contracts.Datas
+namespace SmartCloud.Common.Datas
 {
     public interface IDataAppService : ICrudAppService<DataDto, Guid, GetDataListDto, DataDto>
     {
