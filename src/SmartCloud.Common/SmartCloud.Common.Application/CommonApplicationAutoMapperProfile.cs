e9f0bde3514b955451a1b6d82cddf1eb2b6192ea
molwhy <molw_smartcloud@hotmail.com>
2022-05-30 18:05:39 +0800

    增加实体映射

--- src/SmartCloud.Common/SmartCloud.Common.Application/CommonApplicationAutoMapperProfile.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Application/CommonApplicationAutoMapperProfile.cs
@@ -1,13 +1,18 @@
 ﻿using AutoMapper;
+using SmartCloud.Common.DataIndexs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SmartCloud.Common.Application
+namespace SmartCloud.Common
 {
     public class CommonApplicationAutoMapperProfile : Profile
     {
+        public CommonApplicationAutoMapperProfile()
+        {
+            CreateMap<DataIndex, DataIndexDto>();
+        }
     }
 }
