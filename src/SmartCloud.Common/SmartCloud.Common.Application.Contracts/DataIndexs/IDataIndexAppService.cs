df2f1e4f37f36044118050956728622d64082673
molwhy <molw_smartcloud@hotmail.com>
2022-05-30 16:11:17 +0800

    修改接口

--- src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/DataIndexs/IDataIndexAppService.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/DataIndexs/IDataIndexAppService.cs
@@ -7,8 +7,42 @@ using Volo.Abp.Application.Services;
 
 namespace SmartCloud.Common.DataIndexs
 {
-    public interface IDataIndexAppService : ICrudAppService<DataIndexDto, Guid, DataIndexDto, CreateDataIndexDto, DataIndexDto>
+    public interface IDataIndexAppService : IApplicationService
     {
-        
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <param name="input">类别信息</param>
+        /// <returns>数据字典类别信息</returns>
+        Task<DataIndexDto> CreateAsync(CreateDataIndexDto input);
+
+        /// <summary>
+        /// 修改名称
+        /// </summary>
+        /// <param name="id">Id</param>
+        /// <param name="input">类别信息</param>
+        /// <returns>数据字典类别信息</returns>
+        Task<DataIndexDto> UpdateAsync(Guid id, DataIndexDto input);
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id">类别Id</param>
+        /// <returns>类别信息</returns>
+        Task<bool> DeleteAsync(Guid id);
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="id">Id</param>
+        /// <returns>类别信息</returns>
+        Task<DataIndexDto> GetAsync(Guid id);
+
+        /// <summary>
+        /// 查询列表
+        /// </summary>
+        /// <param name="name">读者姓名</param>
+        /// <returns>类别信息列表</returns>
+        Task<List<DataIndexDto>> GetListAsync(string name);
     }
 }
