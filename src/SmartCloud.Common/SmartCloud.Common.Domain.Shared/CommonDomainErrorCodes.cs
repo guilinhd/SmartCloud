a0e99817694def27a5cc588f2c6c6b1eae64f01b
molwhy <molw_smartcloud@hotmail.com>
2022-05-30 11:35:13 +0800

    修改数据字典类别出错提示

--- src/SmartCloud.Common/SmartCloud.Common.Domain.Shared/CommonDomainErrorCodes.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Domain.Shared/CommonDomainErrorCodes.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SmartCloud.Common.Domain.Shared
+namespace SmartCloud.Common
 {
     public static class CommonDomainErrorCodes
     {
