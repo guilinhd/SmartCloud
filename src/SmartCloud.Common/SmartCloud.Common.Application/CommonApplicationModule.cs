adc0a9371aadb53c72cfe6fe04790ce77cb1aa5e
molwhy <molw_smartcloud@hotmail.com>
2022-05-30 15:42:43 +0800

    新增应用服务层类库项目，并引用领域层、应用共享层项目

---
+++ src/SmartCloud.Common/SmartCloud.Common.Application/CommonApplicationModule.cs
@@ -0,0 +1,24 @@
+﻿
+using Volo.Abp.Modularity;
+using Volo.Abp.AutoMapper;
+using SmartCloud.Common.Application.Contracts;
+using SmartCloud.Common.Domain;
+
+namespace SmartCloud.Common.Application
+{
+    [DependsOn(
+        typeof(AbpAutoMapperModule),
+        typeof(CommonDomainModule),
+        typeof(CommonApplicationContractsModule)
+    )]
+    public class CommonApplicationModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpAutoMapperOptions>(options =>
+            {
+                options.AddMaps<CommonApplicationModule>();
+            });
+        }
+    }
+}
