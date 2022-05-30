9f398f30521401fccf1bfc5c6764370b737749a6
molwhy <molw_smartcloud@hotmail.com>
2022-05-30 14:58:03 +0800

    新增应用契约层类库项目

---
+++ src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/CommonApplicationContractsModule.cs
@@ -0,0 +1,13 @@
+﻿using SmartCloud.Common.Domain.Shared;
+using Volo.Abp.Modularity;
+
+namespace SmartCloud.Common.Application.Contracts
+{
+    [DependsOn(
+        typeof(CommonDomainSharedModule)
+    )]
+    public class CommonApplicationContractsModule : AbpModule
+    {
+
+    }
+}
