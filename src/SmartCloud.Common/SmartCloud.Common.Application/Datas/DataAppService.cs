db889624445517da52f6b1f7422963c2f2984f38
weijhy <weij_smartcloud@hotmail.com>
2022-05-30 19:15:25 +0800

    完成数据字典应用层的功能

---
+++ src/SmartCloud.Common/SmartCloud.Common.Application/Datas/DataAppService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace SmartCloud.Common.Datas
+{
+    public class DataAppService : CrudAppService<Data, DataDto, Guid, GetDataListDto, DataDto>, IDataAppService
+    {
+        public DataAppService(IDataRepository dataRepository) : base(dataRepository)
+        {
+
+        }
+    }
+}
