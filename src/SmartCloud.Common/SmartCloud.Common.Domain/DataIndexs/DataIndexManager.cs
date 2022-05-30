3d14ea105b6873c727794af76d39d6d8a6f7456c
molwhy <molw_smartcloud@hotmail.com>
2022-05-30 17:53:15 +0800

    增加修改描述信息、删除2个方法，保证数据完整性

--- src/SmartCloud.Common/SmartCloud.Common.Domain/DataIndexs/DataIndexManager.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Domain/DataIndexs/DataIndexManager.cs
@@ -2,29 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using SmartCloud.Common.DataIndexs;
+using SmartCloud.Common.Datas;
 using Volo.Abp;
+using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Domain.Services;
 
 namespace SmartCloud.Common.DataIndexs
 {
     public class DataIndexManager : DomainService
     {
-        private IDataIndexRepository _dataIndexRepository;
+        private IDataIndexRepository _repository;
+        private readonly IDataRepository _dataRepository;
 
-        public DataIndexManager(IDataIndexRepository dataIndexRepository)
+        public DataIndexManager(
+            IDataIndexRepository repository,
+            IDataRepository dataRepository)
         {
-            _dataIndexRepository = dataIndexRepository;
+            _repository = repository;
+            _dataRepository = dataRepository;
         }
 
+        /// <summary>
+        /// 创建类别实体
+        /// </summary>
+        /// <param name="name">类别名称</param>
+        /// <returns>类别实体</returns>
+        /// <exception cref="DataIndexAlreadyExistsException">类别名称重复</exception>
         public async Task<DataIndex> Create([NotNull] string name
             )
         {
             Check.NotNullOrWhiteSpace(name, nameof(name));
 
-            var existingAuthor = await _dataIndexRepository.FindByNameAsync(name);
+            var existingAuthor = await _repository.FindByNameAsync(name);
             if (existingAuthor != null)
             {
                 throw new DataIndexAlreadyExistsException(name);
@@ -36,6 +50,12 @@ namespace SmartCloud.Common.DataIndexs
             );
         }
 
+        /// <summary>
+        /// 修改类别名称
+        /// </summary>
+        /// <param name="dataIndex">类别实体</param>
+        /// <param name="newName">类别新名称</param>
+        /// <exception cref="DataIndexAlreadyExistsException">类别名称重复</exception>
         public async Task ChangeNameAsync(
             [NotNull] DataIndex dataIndex,
             [NotNull] string newName)
@@ -43,7 +63,7 @@ namespace SmartCloud.Common.DataIndexs
             Check.NotNull(dataIndex, nameof(dataIndex));
             Check.NotNullOrWhiteSpace(newName, nameof(newName));
 
-            var existingDataIndex = await _dataIndexRepository.FindByNameAsync(newName);
+            var existingDataIndex = await _repository.FindByNameAsync(newName);
             if (existingDataIndex != null && existingDataIndex.Id != dataIndex.Id)
             {
                 throw new DataIndexAlreadyExistsException(newName);
@@ -52,6 +72,43 @@ namespace SmartCloud.Common.DataIndexs
             dataIndex.ChangeName(newName);
         }
 
-        
+        /// <summary>
+        /// 修改类别描述信息
+        /// </summary>
+        /// <param name="id">类别id</param>
+        /// <param name="descriptions">描述信息</param>
+        public async Task UpdateDescriptionsAsync(Guid id, List<Description> descriptions)
+        {
+            var dataIndex = await _repository.GetAsync(id);
+
+            if (dataIndex != null)
+            {
+                dataIndex.Description = JsonSerializer.Serialize(
+                    descriptions,
+                    new JsonSerializerOptions() { Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(UnicodeRanges.All) }
+                );
+                await _repository.UpdateAsync(dataIndex);
+            }
+        }
+
+        /// <summary>
+        /// 删除类别
+        /// </summary>
+        /// <param name="id">类别id<</param>
+        /// <exception cref="DataIndexHasDatasException">有数据字典信息,不能删除</exception>
+        public async Task DeleteAsync(Guid id)
+        {
+            var dataIndex = await _repository.GetAsync(id);
+            if (dataIndex != null)
+            {
+                var datas = await _dataRepository.FindAllAsync(dataIndex.Name);
+                if (datas.Count > 0)
+                {
+                    throw new DataIndexHasDatasException(dataIndex.Name);
+                }
+
+                await _repository.DeleteAsync(dataIndex);
+            }
+        }
     }
 }
