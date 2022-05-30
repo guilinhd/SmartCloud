1c7b29a4b650e7a610cf7a1d99be20d7d17d85ca
molwhy <molw_smartcloud@hotmail.com>
2022-05-30 10:37:27 +0800

    添加数据字典 错误类型

---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain.Shared/CommonDomainErrorCodes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCloud.Common.Domain.Shared
+{
+    public static class CommonDomainErrorCodes
+    {
+        public const string DataIndexAlreadyExists = "TravleCrm:00001";
+
+        public const string DataIndexHasDatas = "TravleCrm:00002";
+    }
+}
