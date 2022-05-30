b84fc5a511b9261c5965091a74f3413b552275cb
molwhy <molw_smartcloud@hotmail.com>
2022-05-30 10:35:58 +0800

    增加数据字典类别 描述类

---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain.Shared/DataIndexs/Description.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCloud.Common.DataIndexs
+{
+    public class Description
+    {
+        public int No { set; get; }
+
+        public int Width { set; get; }
+
+        public string Name { set; get; }
+
+        public string Title { set; get; }
+
+        public string Content { set; get; }
+    }
+}
