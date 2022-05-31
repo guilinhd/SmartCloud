58c0819a85e7ebc6eb8dfe6664f49a03806fcf08
molwhy <molw_smartcloud@hotmail.com>
2022-05-31 19:26:06 +0800

    增加服务配置

--- src/SmartCloud.Common/SmartCloud.Common.Domain/CommonDomainModule.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Domain/CommonDomainModule.cs
@@ -1,4 +1,6 @@
 ﻿using SmartCloud.Common.Domain.Shared;
+using System.Text.Json;
+using System.Text.Unicode;
 using Volo.Abp.Modularity;
 
 namespace SmartCloud.Common.Domain
@@ -6,6 +8,9 @@ namespace SmartCloud.Common.Domain
     [DependsOn(typeof(CommonDomainSharedModule))]
     public class CommonDomainModule : AbpModule
     {
-
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            
+        }
     }
 }
