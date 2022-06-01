13e1dcc7674097d7861cc9755eb5f1f1032a977c
molwhy <molw_smartcloud@hotmail.com>
2022-06-01 12:06:06 +0800

    添加方法注释

--- src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/Datas/IDataAppService.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/Datas/IDataAppService.cs
@@ -10,10 +10,28 @@ namespace SmartCloud.Common.Datas
 {
     public interface IDataAppService : ICrudAppService<DataDto, Guid, GetDataListDto, DataDto>
     {
+        /// <summary>
+        /// 按类别名称查询
+        /// </summary>
+        /// <param name="category">类别名称</param>
+        /// <returns>数据字典信息列表</returns>
         Task<List<DataDto>> FindAllAsync(string category);
 
+        /// <summary>
+        /// 按类别名称、数据字典名称查询
+        /// </summary>
+        /// <param name="category">类别名称</param>
+        /// <param name="name">数据字典名称</param>
+        /// <returns>数据字典信息列表</returns>
         Task<List<DataDto>> FindAllAsync(string category, string name);
 
+        /// <summary>
+        /// 按类别名称、数据字典名称、数据字典备注查询
+        /// </summary>
+        /// <param name="category">类别名称</param>
+        /// <param name="name">数据字典名称</param>
+        /// <param name="remark">数据字典备注</param>
+        /// <returns>数据字典信息列表</returns>
         Task<List<DataDto>> FindAllAsync(string category, string name, string remark);
     }
 }
