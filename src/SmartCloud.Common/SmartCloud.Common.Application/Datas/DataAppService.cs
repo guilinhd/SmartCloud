d94bb7238f1102eaaf5a6518f14af85545624e70
molwhy <molw_smartcloud@hotmail.com>
2022-06-01 11:52:48 +0800

    实现了自定义获取数据字典信息的方法

--- src/SmartCloud.Common/SmartCloud.Common.Application/Datas/DataAppService.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Application/Datas/DataAppService.cs
@@ -9,9 +9,29 @@ namespace SmartCloud.Common.Datas
 {
     public class DataAppService : CrudAppService<Data, DataDto, Guid, GetDataListDto, DataDto>, IDataAppService
     {
+        private readonly IDataRepository _dataRepository;
+
         public DataAppService(IDataRepository dataRepository) : base(dataRepository)
         {
+            _dataRepository = dataRepository;
+        }
 
+        public async Task<List<DataDto>> FindAllAsync(string category)
+        {
+            var datas = await _dataRepository.FindAllAsync(category);
+            return ObjectMapper.Map<List<Data>, List<DataDto>>(datas);
+        }
+
+        public async Task<List<DataDto>> FindAllAsync(string category, string name)
+        {
+            var datas = await _dataRepository.FindAllAsync(category, name);
+            return ObjectMapper.Map<List<Data>, List<DataDto>>(datas);
+        }
+
+        public async Task<List<DataDto>> FindAllAsync(string category, string name, string remark)
+        {
+            var datas = await _dataRepository.FindAllAsync(category, name, remark);
+            return ObjectMapper.Map<List<Data>, List<DataDto>>(datas);
         }
     }
 }
