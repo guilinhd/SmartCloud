2d430f91ea8be06d7bda7c03b5e2179b6070a7fd
molwhy <molw_smartcloud@hotmail.com>
2022-06-01 22:21:44 +0800

    增加查询枚举类型，统一接口

---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain.Shared/DataIndexs/QueryEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCloud.Common.DataIndexs
+{
+    public enum QueryEnum
+    {
+        Reader, //按读者查询
+        Single, //按名称查询
+        All     //
+    }
+}
