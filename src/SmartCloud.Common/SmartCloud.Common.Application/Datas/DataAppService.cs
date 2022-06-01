35f51de275d6fd432e065a2c9b3fa663cea3edab
molwhy <molw_smartcloud@hotmail.com>
2022-06-01 16:53:59 +0800

    添加按类别和备注1查询的方法

--- src/SmartCloud.Common/SmartCloud.Common.Application/Datas/DataAppService.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Application/Datas/DataAppService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace SmartCloud.Common.Datas
 {
@@ -22,7 +23,7 @@ namespace SmartCloud.Common.Datas
         /// </summary>
         /// <param name="category">类别名称</param>
         /// <returns>数据字典信息列表</returns>
-        [Route("api/common/data/category")]
+        [Route("api/common/data/category/{category}")]
         public async Task<List<DataDto>> GetListAsync(string category)
         {
             var datas = await _dataRepository.GetListAsync(category);
@@ -35,7 +36,7 @@ namespace SmartCloud.Common.Datas
         /// <param name="category">类别名称</param>
         /// <param name="name">数据字典名称</param>
         /// <returns>数据字典信息列表</returns>
-        [Route("api/common/data/{category}/name")]
+        [Route("api/common/data/category/{category}/name/{name}")]
         public async Task<List<DataDto>> GetListAsync(string category, string name)
         {
             var datas = await _dataRepository.GetListAsync(category, name);
@@ -49,13 +50,24 @@ namespace SmartCloud.Common.Datas
         /// <param name="name">数据字典名称</param>
         /// <param name="remark">数据字典备注</param>
         /// <returns>数据字典信息列表</returns>
-        [Route("api/common/data/{category}/name/remark")]
-        public async Task<List<DataDto>> GetListAsync(string category, string? name, string remark)
+        [Route("api/common/data/category/{category}/name/{name}/remark/{remark}")]
+        public async Task<List<DataDto>> GetListAsync(string category, string name, string remark)
         {
             var datas = await _dataRepository.GetListAsync(category, name, remark);
             return ObjectMapper.Map<List<Data>, List<DataDto>>(datas);
         }
 
-        
+        /// <summary>
+        /// 按类别名称、数据字典备注查询
+        /// </summary>
+        /// <param name="category">类别名称</param>
+        /// <param name="remark">数据字典备注</param>
+        /// <returns>数据字典信息列表</returns>
+        [Route("api/common/data/category/{category}/remark/{remark}")]
+        public async Task<List<DataDto>> GetListRemarkAsync(string category, string remark)
+{
+            var datas = await _dataRepository.GetListRemarkAsync(category, remark);
+            return ObjectMapper.Map<List<Data>, List<DataDto>>(datas);
+        }
     }
 }
