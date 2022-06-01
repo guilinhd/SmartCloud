01f61afff6737b06781e16ea573e154c09164b11
molwhy <molw_smartcloud@hotmail.com>
2022-06-01 22:20:36 +0800

    完成数据字典类型应用层功能

--- src/SmartCloud.Common/SmartCloud.Common.Application/DataIndexs/DataIndexAppService.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Application/DataIndexs/DataIndexAppService.cs
@@ -9,10 +9,13 @@ using System.Threading.Tasks;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
 using Volo.Abp.ObjectMapping;
-
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace SmartCloud.Common.DataIndexs
 {
+    
+    
     public class DataIndexAppService : ApplicationService, IDataIndexAppService
     {
         private readonly IDataIndexRepository _repository;
@@ -32,6 +35,7 @@ namespace SmartCloud.Common.DataIndexs
         /// </summary>
         /// <param name="name">名称</param>
         /// <returns>数据字典类别信息</returns>
+        [Route("api/common/dataindex/{name}")]
         public async Task<DataIndexDto> CreateAsync(string name)
         {
             DataIndex dataIndex = await _manager.Create(name);
@@ -46,6 +50,7 @@ namespace SmartCloud.Common.DataIndexs
         /// </summary>
         /// <param name="id">类别Id</param>
         /// <returns>类别信息</returns>
+        [Route("api/common/dataindex/{id}")]
         public async Task DeleteAsync(Guid id)
         {
             await _manager.DeleteAsync(id);
@@ -56,6 +61,7 @@ namespace SmartCloud.Common.DataIndexs
         /// </summary>
         /// <param name="id">Id</param>
         /// <returns>类别信息</returns>
+        [Route("api/common/dataindex/{id}")]
         public async Task<DataIndexDto> GetAsync(Guid id)
         {
             var dataIndex = await _repository.GetAsync(id);
@@ -68,13 +74,22 @@ namespace SmartCloud.Common.DataIndexs
         }
 
         /// <summary>
-        /// 查询列表
+        /// 显示当前读者可以查看的信息列表
         /// </summary>
         /// <param name="name">读者姓名</param>
         /// <returns>类别信息列表</returns>
+        [Route("api/common/dataindex/")]
         public async Task<List<DataIndexDto>> GetListAsync(string? name)
         {
-            var datas = await _repository.FindAllByNameAsync(name);
+            var datas = new List<DataIndex>();
+            if (name.IsNullOrEmpty())
+            {
+                datas = await _repository.GetLisAsync(QueryEnum.All);
+            }
+            else
+            {
+                datas = await _repository.GetLisAsync(QueryEnum.Reader, name);
+            }
             return ObjectMapper.Map<List<DataIndex>, List<DataIndexDto>>(datas);
         }
 
@@ -84,6 +99,7 @@ namespace SmartCloud.Common.DataIndexs
         /// <param name="id">Id</param>
         /// <param name="name">名称</param>
         /// <returns>数据字典类别信息</returns>
+        [Route("api/common/dataindex/id/{id}/name/{name}")]
         public async Task<DataIndexDto> UpdateAsync(Guid id, string name)
         {
             var dataIndex = await _repository.GetAsync(id);
@@ -91,7 +107,6 @@ namespace SmartCloud.Common.DataIndexs
             if (dataIndex != null)
             {
                 await _manager.ChangeNameAsync(dataIndex, name);
-                await _repository.UpdateAsync(dataIndex);
                 return ObjectMapper.Map<DataIndex, DataIndexDto>(dataIndex);
             }
 
@@ -102,16 +117,16 @@ namespace SmartCloud.Common.DataIndexs
         /// 修改权限
         /// </summary>
         /// <param name="id">id</param>
-        /// <param name="reader">读者</param>
-        /// <param name="editor">编辑者</param>
-        public async Task UpdateAuthorization(Guid id, string reader, string editor)
+        /// <param name="authority">权限</param>
+        [Route("api/common/dataindex/update/{id}")]
+        public async Task UpdateAsync(Guid id, AuthorityDto authority)
         {
             var dataIndex = await _repository.GetAsync(id);
             if (dataIndex != null)
             {
-                dataIndex.Reader = reader;
-                dataIndex.Editor = editor;
-
+                dataIndex.Reader = authority.Readers.Count == 0 ? "" : authority.Readers.JoinAsString(";") + ";";
+                dataIndex.Editor = authority.Editors.Count == 0  ? "" : authority.Editors.JoinAsString(";") + ";";
+                
                 await _repository.UpdateAsync(dataIndex);
             }
         }
@@ -122,9 +137,11 @@ namespace SmartCloud.Common.DataIndexs
         /// <param name="id">id</param>
         /// <param name="descriptions">描述信息</param>
         /// <returns></returns>
-        public async Task UpdateDescriptionsAsync(Guid id, List<Description> descriptions)
+        [HttpPost]
+        [Route("api/common/dataindex/update/{id}")]
+        public async Task UpdateAsync(Guid id, List<Description> descriptions)
         {
-            await _manager.UpdateDescriptionsAsync(id, descriptions);
+            await _manager.UpdateAsync(id, descriptions);
         }
     }
 }
