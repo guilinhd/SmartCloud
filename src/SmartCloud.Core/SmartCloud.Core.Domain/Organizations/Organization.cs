b61dd1289c906c631fcfb142c0b7e3b6a95dcf65
molwhy <molw_smartcloud@hotmail.com>
2022-06-02 16:29:15 +0800

    添加组织结构领域实体

---
+++ src/SmartCloud.Core/SmartCloud.Core.Domain/Organizations/Organization.cs
@@ -0,0 +1,25 @@
+﻿using Volo.Abp.Domain.Entities;
+
+namespace SmartCloud.Core.Organizations
+{
+    public class Organization : AggregateRoot<Guid>
+    {
+        public int No { get; set; }
+
+        public string ParentId { get; set; } = null!;
+
+        public int Category { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Type { get; set; } = "";
+
+        public string Phone { get; set; } = "";
+
+        public string Fax { get; set; } = "";
+
+        public string Accounting { get; set; } = null!;
+
+        public List<Description> Descriptions { get; set; } = new List<Description>();
+    }
+}
