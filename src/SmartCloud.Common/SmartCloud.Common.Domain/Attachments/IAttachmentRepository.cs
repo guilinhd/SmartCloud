974fd0e5a1448804409f7507dbc5b9bcd407f9ae
molwhy <molw_smartcloud@hotmail.com>
2022-06-02 11:04:06 +0800

    添加扩展接口，自定义获取附件列表方法

---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain/Attachments/IAttachmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using SmartCloud.Common.DataIndexs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+
+namespace SmartCloud.Common.Attachments
+{
+    public interface IAttachmentRepository : IRepository<Attachment, Guid>
+    {
+        Task<List<Attachment>> GetListAsync(QueryEnum query, string tableId, string name);
+    }
+}
