c0958eff73daf665f0ad7cd8ff1b1c3de30c3b1f
molwhy <molw_smartcloud@hotmail.com>
2022-06-02 16:28:55 +0800

    添加自定义描述类

---
+++ src/SmartCloud.Core/SmartCloud.Core.Domain.Shared/Description.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCloud.Core;
+
+public class Description
+{
+    public int No { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Value { get; set; } = "";
+}
