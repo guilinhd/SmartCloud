708b7bf52674693eb845443aebddf9247b5c2d21
molwhy <molw_smartcloud@hotmail.com>
2022-06-02 17:46:45 +0800

    添加组织结构repository的实现

---
+++ src/SmartCloud.Core/SmartCloud.Core.EntityFrameworkCore/Organizations/EfOrganizationRepository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartCloud.Core.EntityFrameworkCore;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace SmartCloud.Core.Organizations
+{
+    public class EfOrganizationRepository : EfCoreRepository<CoreDbContext, Organization, Guid>, IOrganizationRepository
+    {
+        public EfOrganizationRepository(IDbContextProvider<CoreDbContext> dbContextProvider) : base(dbContextProvider)
+        {
+        }
+
+        public async Task<List<Organization>> GetListAsync(QueryEnum query, string name)
+        {
+            var dbSet = await GetDbSetAsync();
+
+            switch (query)
+            {
+                case QueryEnum.Parent:
+                    return await dbSet.Where(d => d.ParentId == name).ToListAsync();
+                case QueryEnum.Name:
+                    {
+                        var organizations = new List<Organization>();
+                        var organization = await dbSet.Where(d => d.Name == name).FirstOrDefaultAsync();
+                        if (organization != null)
+                        {
+                            organizations.Add(organization);
+                        }
+                        return organizations;
+                    }
+                case QueryEnum.Type:
+                    return await dbSet.Where(d => d.Type == name).ToListAsync();
+                case QueryEnum.Category:
+                    return await dbSet.Where(d => d.Category == Convert.ToInt16(name)).ToListAsync();
+                case QueryEnum.Accounting:
+                    return await dbSet.Where(d => d.Accounting.StartsWith(name)).ToListAsync();
+                default:
+                    return await dbSet.ToListAsync();
+            }
+        }
+    }
+}
