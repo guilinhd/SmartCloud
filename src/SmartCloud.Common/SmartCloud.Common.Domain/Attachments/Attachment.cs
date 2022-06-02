1fe7a39fcd042e2502d8d06f5582c551c3f42dfb
molwhy <molw_smartcloud@hotmail.com>
2022-06-02 11:01:42 +0800

    新增附件领域实体

---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain/Attachments/Attachment.cs
@@ -0,0 +1,31 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace SmartCloud.Common.Attachments
+{
+    public class Attachment : AuditedAggregateRoot<Guid>
+    {
+        [Required]
+        public string TableId { get; set; }
+
+        [Required]
+        public string TableName { get; set; }
+
+        public string Name { get; set; }
+
+        public string FullName { get; set; }
+
+        public string LastWriteTimeUtc { get; set; }
+
+        public string Length { get; set; }
+
+        public string Extension { get; set; }
+
+        public string ServerPathName { get; set; }
+
+        public string ServerFileName { get; set; }
+
+        public int Status { get; set; }
+    }
+}
