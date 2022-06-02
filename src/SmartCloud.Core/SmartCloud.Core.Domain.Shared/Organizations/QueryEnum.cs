39b3b74a65902c48896eea1473ab70ef68f37492
molwhy <molw_smartcloud@hotmail.com>
2022-06-02 16:53:55 +0800

    添加组织结构查询枚举，统一查询方法

---
+++ src/SmartCloud.Core/SmartCloud.Core.Domain.Shared/Organizations/QueryEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCloud.Core.Organizations
+{
+    public enum QueryEnum
+    {
+        Parent,
+        Name,
+        Type,
+        Category,
+        Accounting
+    }
+}
