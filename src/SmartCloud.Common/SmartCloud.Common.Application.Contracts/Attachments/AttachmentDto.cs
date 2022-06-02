c2acfe5d92acac6dde761e436eb175f877f7fc2a
molwhy <molw_smartcloud@hotmail.com>
2022-06-02 14:12:29 +0800

    完成附件应用契约层相关功能

---
+++ src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/Attachments/AttachmentDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+
+namespace SmartCloud.Common.Attachments
+{
+    public class AttachmentDto : AuditedEntityDto<Guid>
+    {
+        [Required]
+        public string TableId { get; set; }
+
+        public string TableName { get; set; }
+
+        public string Name { get; set; }
+
+        public string FullName { get; set; }
+
+        public string LastWriteTimeUtc { get; set; }
+
+        public string Length { get; set; }
+
+        public string Extension { get; set; }
+
+        public string ServerPathName { get; set; }
+
+        public string ServerFileName { get; set; }
+
+        public int Status { get; set; }
+    }
+}
---
+++ src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/Attachments/IAttachmentService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace SmartCloud.Common.Attachments
+{
+    public interface IAttachmentService : ICrudAppService<AttachmentDto, Guid>
+    {
+        /// <summary>
+        /// 查询当前行的附件信息
+        /// </summary>
+        /// <param name="tableId">行id</param>
+        /// <returns></returns>
+        Task<List<AttachmentDto>> GetAsync(string tableId);
+
+        /// <summary>
+        /// 查询当前行、文件的附件信息
+        /// </summary>
+        /// <param name="tableId">行id</param>
+        /// <param name="serverFileName">文件名</param>
+        /// <returns></returns>
+        Task<List<AttachmentDto>> GetAsync(string tableId, string serverFileName);
+
+        /// <summary>
+        /// 批量查询行的附件信息
+        /// </summary>
+        /// <param name="tableIds">行id</param>
+        /// <returns></returns>
+        Task<List<AttachmentDto>> GetListAsync(string tableIds);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tableId"></param>
+        /// <returns></returns>
+        Task DeleteAsync(string tableId);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tableId"></param>
+        /// <param name="serverFileName"></param>
+        /// <returns></returns>
+        Task DeleteAsync(string tableId, string serverFileName);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tableId"></param>
+        /// <param name="serverPathName"></param>
+        /// <returns></returns>
+        Task DeleteListAsync(string tableId, string serverPathName);
+    }
+}
