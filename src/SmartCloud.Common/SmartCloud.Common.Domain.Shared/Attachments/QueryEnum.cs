fc70d192a19f3d7228321739c9b4bf1aedc5bb14
molwhy <molw_smartcloud@hotmail.com>
2022-06-02 11:01:25 +0800

    添加枚举类型，统一获取附件列表的接口

---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain.Shared/Attachments/QueryEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCloud.Common.Attachments
+{
+    public enum QueryEnum
+    {   
+        File,
+        Folder,
+        Row,
+        Rows
+    }
+}
