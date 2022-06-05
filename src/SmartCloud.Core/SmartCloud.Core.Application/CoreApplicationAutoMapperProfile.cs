7cb040b5ba71d350dca741071f0960a33da0532e
molwhy <molw_smartcloud@hotmail.com>
2022-06-05 18:20:10 +0800

    添加应用层类库项目，并添加包和项目引用

---
+++ src/SmartCloud.Core/SmartCloud.Core.Application/CoreApplicationAutoMapperProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using SmartCloud.Core.Organizations;
+using System.Text.Json;
+
+namespace SmartCloud.Core
+{
+    public class CoreApplicationAutoMapperProfile : Profile
+    {
+        public CoreApplicationAutoMapperProfile()
+        {
+            CreateMap<Organization, OrganizationDto>()
+                .ForMember(
+                des => des.Descriptions,
+                opt => opt.MapFrom(src => src.Description.ToDescriptions()));
+        }
+    }
+
+    public static class ProfileExtensions
+    {
+        public static List<Description> ToDescriptions(this string description)
+        {
+            return JsonSerializer.Deserialize<List<Description>>(description);
+        }
+    }
+}
---
+++ src/SmartCloud.Core/SmartCloud.Core.Application/CoreApplicationModule.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.Modularity;
+
+namespace SmartCloud.Core
+{
+    [DependsOn(
+        typeof(CoreDomainModule),
+        typeof(CoreApplicationContractsModule)
+    )]
+    public class CoreApplicationModule : AbpModule
+    {
+
+    }
+}
