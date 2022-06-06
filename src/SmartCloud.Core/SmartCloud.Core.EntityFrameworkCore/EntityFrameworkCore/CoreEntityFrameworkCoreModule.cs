9e1cff30c08aed64168d709ec3494ceb14b2a68a
molwhy <molw_smartcloud@hotmail.com>
2022-06-06 16:09:02 +0800

    配置mysql服务

--- src/SmartCloud.Core/SmartCloud.Core.EntityFrameworkCore/EntityFrameworkCore/CoreEntityFrameworkCoreModule.cs
+++ src/SmartCloud.Core/SmartCloud.Core.EntityFrameworkCore/EntityFrameworkCore/CoreEntityFrameworkCoreModule.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore.MySQL;
 using Volo.Abp.Modularity;
 
@@ -9,6 +11,21 @@ namespace SmartCloud.Core.EntityFrameworkCore
     )]
     public class CoreEntityFrameworkCoreModule : AbpModule
     {
-        
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddAbpDbContext<CoreDbContext>(options =>
+            {
+                /* Remove "includeAllEntities: true" to create
+                 * default repositories only for aggregate roots */
+                options.AddDefaultRepositories(includeAllEntities: true);
+            });
+
+            Configure<AbpDbContextOptions>(options =>
+            {
+                /* The main point to change your DBMS.
+                 * See also TravelCrmMigrationsDbContextFactory for EF Core tooling. */
+                options.UseMySQL();
+            });
+        }
     }
 }
