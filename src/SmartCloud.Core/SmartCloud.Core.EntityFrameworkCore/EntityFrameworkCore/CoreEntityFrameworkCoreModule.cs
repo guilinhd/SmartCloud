0f5c93aeed724fb03f82fe519689bc5d2415722c
molwhy <molw_smartcloud@hotmail.com>
2022-06-07 14:38:23 +0800

    添加commonentityframeworkcoremodule的依赖

--- src/SmartCloud.Core/SmartCloud.Core.EntityFrameworkCore/EntityFrameworkCore/CoreEntityFrameworkCoreModule.cs
+++ src/SmartCloud.Core/SmartCloud.Core.EntityFrameworkCore/EntityFrameworkCore/CoreEntityFrameworkCoreModule.cs
@@ -3,11 +3,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore.MySQL;
 using Volo.Abp.Modularity;
+using SmartCloud.Common.EntityFrameworkCore;
 
 namespace SmartCloud.Core.EntityFrameworkCore
 {
     [DependsOn(
-        typeof(AbpEntityFrameworkCoreMySQLModule)
+        typeof(AbpEntityFrameworkCoreMySQLModule),
+        typeof(CommonEntityFrameworkCoreModule)
     )]
     public class CoreEntityFrameworkCoreModule : AbpModule
     {
