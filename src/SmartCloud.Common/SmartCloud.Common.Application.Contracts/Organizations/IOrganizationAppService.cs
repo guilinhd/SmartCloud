9d17f038dc0d71aa79ecc07e83a754a43e7fd111
molwhy <molw_smartcloud@hotmail.com>
2022-06-08 11:18:41 +0800

    添加组织结构应用契约层功能

---
+++ src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/Organizations/IOrganizationAppService.cs
@@ -0,0 +1,58 @@
+﻿
+
+using SmartCloud.Common.Datas;
+using Volo.Abp.Application.Services;
+
+namespace SmartCloud.Common.Organizations
+{
+    public interface IOrganizationAppService : IApplicationService
+    {
+        /// <summary>
+        /// 新增保存
+        /// </summary>
+        /// <param name="dto">实体</param>
+        /// <returns></returns>
+        Task<OrganizationDto> CreateAsync(OrganizationDto dto);
+
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <returns>数据字典</returns>
+        Task<GetDataNameListDto> CreateAsync();
+
+        /// <summary>
+        /// 修改保存
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <param name="dto">实体</param>
+        /// <returns></returns>
+        Task UpdateAsync(Guid id, OrganizationDto dto);
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns></returns>
+        Task DeleteAsync(Guid id);
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns></returns>
+        Task<OrganizationDto> GetAsync(Guid id);
+
+        /// <summary>
+        /// 按上级id查询
+        /// </summary>
+        /// <param name="parentId">上级组织结构id</param>
+        /// <returns>实体列表</returns>
+        Task<List<OrganizationDto>> GetListAsync(string parentId);
+
+        /// <summary>
+        /// 查询全部
+        /// </summary>
+        /// <returns>实体列表</returns>
+        Task<List<OrganizationDto>> GetListAsync();
+    }
+}
---
+++ src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/Organizations/OrganizationDto.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Volo.Abp.Application.Dtos;
+
+namespace SmartCloud.Common.Organizations
+{
+    public class OrganizationDto : AuditedEntityDto<Guid>
+    {
+        public int No { get; set; }
+
+        public string ParentId { get; set; } = null!;
+
+        public int Category { get; set; } = 0;
+
+        public string Name { get; set; } = null!;
+
+        public string Type { get; set; } = "";
+
+        public string Phone { get; set; } = "";
+
+        public string Fax { get; set; } = "";
+
+        public string Accounting { get; set; } = "";
+
+        public List<Description> Descriptions { get; set; } = new List<Description>();
+    }
+}
