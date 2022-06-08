7186472d540a7eb1c8a6ca6f408d6f767f2bed85
molwhy <molw_smartcloud@hotmail.com>
2022-06-08 11:02:55 +0800

    添加组织结构领域服务

---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain/Organizations/OrganizationManager.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.Extensions.Options;
+using System.Text.Json;
+using Volo.Abp.Domain.Services;
+using Volo.Abp;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SmartCloud.Common.Organizations
+{
+    public class OrganizationManager : DomainService
+    {
+        private readonly IOrganizationRepository _repository;
+        private readonly IOptions<JsonSerializerOptions> _options;
+
+        public OrganizationManager(
+            IOrganizationRepository repository,
+            IOptions<JsonSerializerOptions> options)
+        {
+            _repository = repository;
+            _options = options;
+        }
+
+        /// <summary>
+        /// 创建组织结构实体
+        /// </summary>
+        /// <param name="parentId">上级组织结构Id</param>
+        /// <param name="no">序号</param>
+        /// <param name="name">名称</param>
+        /// <param name="type">类型</param>
+        /// <param name="phone">电话</param>
+        /// <param name="fax">传真</param>
+        /// <param name="descriptions">描述信息</param>
+        /// <returns>组织结构实体</returns>
+        /// <exception cref="OrganizationAlreadyExistsException">组织结构名称重复</exception>
+        public async Task<Organization> Create(
+            string parentId,
+            int no,
+            [NotNull] string name,
+            string type,
+            string phone,
+            string fax,
+            List<Description> descriptions
+            )
+        {
+            Check.NotNullOrWhiteSpace(name, nameof(name));
+
+            #region 名称是否重复
+            var existingOrganization = await _repository.GetListAsync(QueryEnum.Name, name);
+            if (existingOrganization.Count > 0)
+            {
+                throw new OrganizationAlreadyExistsException(name);
+            }
+            #endregion
+
+            #region 上级组织结构的核算科目
+            string parentAccounting = ""; int category = 1;
+            if (parentId != "")
+            {
+                var parentOrganization = await _repository.GetAsync(new Guid(parentId));
+                if (parentOrganization != null)
+                {
+                    parentAccounting = parentOrganization.Accounting;
+                    category = parentOrganization.Category + 1;
+                }
+            }
+            #endregion
+
+            #region 生成实体
+            //同级组织结构的核算科目
+            string accounting = parentAccounting + await GetAccountingAsync(parentId);
+
+            return new Organization(
+                GuidGenerator.Create(),
+                parentId,
+                category,
+                no,
+                name,
+                type,
+                phone,
+                fax,
+                accounting,
+                JsonSerializer.Serialize(descriptions, _options.Value)
+            );
+            #endregion
+        }
+
+        /// <summary>
+        /// 修改组织结构实体
+        /// </summary>
+        /// <param name="organization">组织结构实体</param>
+        /// <param name="newName">新名称</param>
+        /// <param name="descriptions">描述信息</param>
+        /// <exception cref="OrganizationAlreadyExistsException">组织结构名称重复</exception>
+        public async Task UpdateAsync(
+            [NotNull] Organization organization,
+            [NotNull] string newName,
+            List<Description> descriptions)
+        {
+            Check.NotNull(organization, nameof(organization));
+            Check.NotNullOrWhiteSpace(newName, nameof(newName));
+
+            var existingOrganization = await _repository.GetListAsync(QueryEnum.Name, newName);
+            if (existingOrganization.Count > 0 && existingOrganization.First().Id != organization.Id)
+            {
+                throw new OrganizationAlreadyExistsException(newName);
+            }
+
+            organization.Description = JsonSerializer.Serialize(descriptions, _options.Value);
+            await _repository.UpdateAsync(organization);
+        }
+
+        /// <summary>
+        /// 删除组织结构实体
+        /// </summary>
+        /// <param name="id">组织结构id</param>
+        /// <returns></returns>
+        /// <exception cref="OrganizationHasChildrenException">组织结构删除失败</exception>
+        public async Task DeleteAsync(Guid id)
+        {
+            var organization = await _repository.GetAsync(id);
+            if (organization != null)
+            {
+                var organizationChildren = await _repository.GetListAsync(QueryEnum.Parent, organization.Id.ToString());
+                if (organizationChildren.Count > 0)
+                {
+                    throw new OrganizationHasChildrenException(organization.Name);
+                }
+
+                await _repository.DeleteAsync(organization);
+            }
+        }
+
+        /// <summary>
+        /// 调整组织结构的所属上级
+        /// </summary>
+        /// <param name="id">组织结构id</param>
+        /// <param name="category">在组织结构所在的层级</param>
+        /// <param name="parentId">上级组织结构id</param>
+        /// <returns></returns>
+        public async Task AdjustAsync(Guid id, int category, string parentId)
+        {
+            var organization = await _repository.GetAsync(id);
+            if (organization.ParentId == parentId)
+            {
+                throw new OrganizationAlreadyExistsException(organization.Name);
+            }
+
+            var parentOrganization = await _repository.GetAsync(new Guid(parentId));
+            if (organization != null)
+            {
+                organization.Category = category;
+                organization.ParentId = parentId;
+                organization.Accounting = parentOrganization.Accounting + await GetAccountingAsync(parentId);
+
+                await _repository.UpdateAsync(organization);
+            }
+        }
+
+        /// <summary>
+        /// 获取当前组织结构的核算编号
+        /// </summary>
+        /// <param name="parentId">上级组织结构</param>
+        /// <returns>核算编号</returns>
+        private async Task<string> GetAccountingAsync(string parentId)
+        {
+            var organizations = await _repository.GetListAsync(QueryEnum.Parent, parentId);
+            if (organizations.Count == 0)
+            {
+                return "0001";
+            }
+            else
+            {
+                var accounting = organizations.Max(d => d.Accounting);
+                return (Convert.ToInt16(accounting.Right(4)) + 1).ToString("d:4");
+            }
+        }
+    }
+}
