43de347519a74eeec2b5e717ea7182dfc3f5379a
molwhy <molw_smartcloud@hotmail.com>
2022-06-08 10:46:15 +0800

    添加组织结构仓储扩展类

---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain/Organizations/IOrganizationRepository.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Domain.Repositories;
+
+namespace SmartCloud.Common.Organizations
+{
+    public interface IOrganizationRepository : IRepository<Organization, Guid>
+    {
+        Task<List<Organization>> GetListAsync(QueryEnum query, string name);
+    }
+}
