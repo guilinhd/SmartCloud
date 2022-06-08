262f7e6ad58069fbbb65f06310561ece490b1e3a
molwhy <molw_smartcloud@hotmail.com>
2022-06-08 11:35:50 +0800

    完善注释，增加查询全部的方法

--- src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/DataIndexs/IDataIndexAppService.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/DataIndexs/IDataIndexAppService.cs
@@ -13,15 +13,42 @@ namespace SmartCloud.Common.DataIndexs
         /// 新增
         /// </summary>
         /// <param name="name">名称</param>
-        /// <returns>数据字典类别信息</returns>
+        /// <returns></returns>
         Task<DataIndexDto> CreateAsync(string name);
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id">Id</param>
+        /// <returns>类别信息</returns>
+        Task DeleteAsync(Guid id);
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="id">Id</param>
+        /// <returns>类别信息</returns>
+        Task<DataIndexDto> GetAsync(Guid id);
+
+        /// <summary>
+        /// 按用户名查询       
+        /// </summary>
+        /// <param name="name">用户名</param>
+        /// <returns>实体列表</returns>
+        Task<List<DataIndexDto>> GetListAsync(string name);
+
+        /// <summary>
+        /// 查询全部
+        /// </summary>
+        /// <returns>实体列表</returns>
+        Task<List<DataIndexDto>> GetListAsync();
 
         /// <summary>
         /// 修改名称
         /// </summary>
         /// <param name="id">Id</param>
         /// <param name="name">名称</param>
-        /// <returns>数据字典类别信息</returns>
+        /// <returns>实体列表</returns>
         Task<DataIndexDto> UpdateAsync(Guid id, string name);
 
         /// <summary>
@@ -39,26 +66,5 @@ namespace SmartCloud.Common.DataIndexs
         /// <param name="authority">权限</param>
         /// <returns></returns>
         Task UpdateAsync(Guid id, AuthorityDto authority);
-
-        /// <summary>
-        /// 删除
-        /// </summary>
-        /// <param name="id">类别Id</param>
-        /// <returns>类别信息</returns>
-        Task DeleteAsync(Guid id);
-
-        /// <summary>
-        /// 查询
-        /// </summary>
-        /// <param name="id">Id</param>
-        /// <returns>类别信息</returns>
-        Task<DataIndexDto> GetAsync(Guid id);
-
-        /// <summary>
-        /// 显示当前读者可以查看的信息列表
-        /// </summary>
-        /// <param name="name">读者姓名</param>
-        /// <returns>类别信息列表</returns>
-        Task<List<DataIndexDto>> GetListAsync(string name);
     }
 }
