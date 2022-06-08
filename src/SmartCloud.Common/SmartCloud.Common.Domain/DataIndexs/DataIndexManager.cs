35cbab578bc47a47dc1d4afe492ea23866e9f218
molwhy <molw_smartcloud@hotmail.com>
2022-06-08 18:01:11 +0800

    删除更新数据库的操作，只保留查询功能

--- src/SmartCloud.Common/SmartCloud.Common.Domain/DataIndexs/DataIndexManager.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Domain/DataIndexs/DataIndexManager.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Text.Unicode;
-using System.Threading.Tasks;
+﻿using System.Text.Json;
 using JetBrains.Annotations;
 using SmartCloud.Common.Datas;
 using Volo.Abp;
-using Volo.Abp.Domain.Repositories;
 using Volo.Abp.Domain.Services;
 using Microsoft.Extensions.Options;
 
@@ -17,30 +10,27 @@ namespace SmartCloud.Common.DataIndexs
     public class DataIndexManager : DomainService
     {
         private readonly IDataIndexRepository _repository;
-        private readonly IDataRepository _dataRepository;
         private readonly DataManager _dataManager;
         private readonly IOptions<JsonSerializerOptions> _options;
 
         public DataIndexManager(
             IDataIndexRepository repository,
-            IDataRepository dataRepository,
             DataManager dataManager,
             IOptions<JsonSerializerOptions> options
-            )
+        )
         {
             _repository = repository;
-            _dataRepository = dataRepository;
             _dataManager = dataManager;
             _options = options;
         }
 
         /// <summary>
-        /// 创建类别实体
+        /// 是否允许创建类别实体
         /// </summary>
         /// <param name="name">类别名称</param>
         /// <returns>类别实体</returns>
         /// <exception cref="DataIndexAlreadyExistsException">类别名称重复</exception>
-        public async Task<DataIndex> Create([NotNull] string name
+        public async Task<DataIndex> CreateAsync([NotNull] string name
             )
         {
             Check.NotNullOrWhiteSpace(name, nameof(name));
@@ -77,11 +67,11 @@ namespace SmartCloud.Common.DataIndexs
                 GuidGenerator.Create(),
                 name,
                 JsonSerializer.Serialize(descriptions, _options.Value)
-                ); 
+            ); 
         }
 
         /// <summary>
-        /// 修改类别名称
+        /// 是否允许修改类别名称
         /// </summary>
         /// <param name="dataIndex">类别实体</param>
         /// <param name="newName">类别新名称</param>
@@ -102,46 +92,21 @@ namespace SmartCloud.Common.DataIndexs
             }
 
             dataIndex.ChangeName(newName);
-            await _repository.UpdateAsync(dataIndex);
-            await _dataManager.ChangeAsync(oldName, newName);
         }
 
         /// <summary>
-        /// 修改类别描述信息
+        /// 是否允许删除类别
         /// </summary>
-        /// <param name="id">类别id</param>
-        /// <param name="descriptions">描述信息</param>
-        public async Task UpdateAsync(Guid id, List<Description> descriptions)
-        {
-            var dataIndex = await _repository.GetAsync(id);
-
-            if (dataIndex != null)
-            {
-                dataIndex.Description = JsonSerializer.Serialize(
-                    descriptions,
-                    new JsonSerializerOptions() { Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(UnicodeRanges.All) }
-                );
-                await _repository.UpdateAsync(dataIndex);
-            }
-        }
-
-        /// <summary>
-        /// 删除类别
-        /// </summary>
-        /// <param name="id">类别id<</param>
+        /// <param name="name">类别名称<</param>
         /// <exception cref="DataIndexHasDatasException">有数据字典信息,不能删除</exception>
-        public async Task DeleteAsync(Guid id)
+        public async Task DeleteAsync([NotNull] string name)
         {
-            var dataIndex = await _repository.GetAsync(id);
-            if (dataIndex != null)
-            {
-                var datas = await _dataRepository.GetListAsync(dataIndex.Name);
-                if (datas.Count > 0)
-                {
-                    throw new DataIndexHasDatasException(dataIndex.Name);
-                }
+            Check.NotNullOrWhiteSpace(name, nameof(name));
 
-                await _repository.DeleteAsync(dataIndex);
+            int count = await _dataManager.GetDatasCount(name);
+            if (count > 0)
+            {
+                throw new DataIndexHasDatasException(name);
             }
         }
     }
