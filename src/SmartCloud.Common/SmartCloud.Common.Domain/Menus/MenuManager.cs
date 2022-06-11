6dc98aae1075662a1cf6fa9dd439325b24609f53
molwhy <molw_smartcloud@hotmail.com>
2022-06-11 16:35:53 +0800

    添加菜单领域服务

---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain/Menus/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+using Volo.Abp;
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Domain.Services;
+
+namespace SmartCloud.Common.Menus
+{
+    public class MenuManager : DomainService
+    {
+        private readonly IMenuRepository _repository;
+
+        public MenuManager(IMenuRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task AdjustAsync(
+            [NotNull] Menu menu, 
+            [NotNull] Menu menuParent)
+        {
+            Check.NotNull(menu, nameof(menu));  
+            Check.NotNull(menuParent, nameof(menuParent));
+
+            menu.Category = menuParent.Category + 1;
+            menu.ParentId = menuParent.ParentId;
+
+            await _repository.UpdateAsync(menu);
+        }
+    }
+}
