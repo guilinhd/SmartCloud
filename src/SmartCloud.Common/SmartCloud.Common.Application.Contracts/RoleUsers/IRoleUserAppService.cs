03f6b0f7926e6ba13b82d2e4772a6768f7948825
molwhy <molw_smartcloud@hotmail.com>
2022-06-11 21:37:21 +0800

    添加角色用户应用契约层功能

---
+++ src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/RoleUsers/IRoleUserAppService.cs
@@ -0,0 +1,12 @@
+﻿
+using Volo.Abp.Application.Services;
+
+namespace SmartCloud.Common.RoleUsers
+{
+    public interface IRoleUserAppService : IApplicationService
+    {
+        Task<List<RoleUserDto>> CreateAsync(CreateRoleUserDto dto);
+
+        Task<List<RoleUserDto>> GetListAsync(string roleId);
+    }
+}
---
+++ src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/RoleUsers/RoleUserDto.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Volo.Abp.Application.Dtos;
+
+namespace SmartCloud.Common.RoleUsers
+{
+    public class RoleUserDto : AuditedEntityDto<Guid>
+    {
+        public string RoleId { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
