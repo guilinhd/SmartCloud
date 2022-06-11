0059ea3907d966113aa0a3fd24b4bf752b086df3
molwhy <molw_smartcloud@hotmail.com>
2022-06-11 21:29:18 +0800

    实现用户角色仓储功能

---
+++ src/SmartCloud.Common/SmartCloud.Common.EntityFrameworkCore/RoleUsers/EfRoleUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartCloud.Common.EntityFrameworkCore;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+
+namespace SmartCloud.Common.RoleUsers
+{
+    public class EfRoleUserRepository : EfCoreRepository<CommonDbContext, RoleUser, Guid>, IRoleUserRepository
+    {
+        public EfRoleUserRepository(IDbContextProvider<CommonDbContext> dbContextProvider) : base(dbContextProvider)
+        {
+        }
+
+        public async Task<List<RoleUser>> GetListAsync(QueryEnum query, string name)
+        {
+            var dbSet = await GetDbSetAsync();
+            return query switch
+            {
+                QueryEnum.RoleId => await dbSet.Where(d => d.RoleId == name).ToListAsync(),
+                _ => await dbSet.ToListAsync(),
+            };
+        }
+    }
+}
