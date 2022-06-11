84fddb59a03346a89bc641d125a005900c9f77ad
molwhy <molw_smartcloud@hotmail.com>
2022-06-11 16:36:34 +0800

    添加调整菜单到新的上级菜单功能

--- src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/Menus/IMenuAppService.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/Menus/IMenuAppService.cs
@@ -4,6 +4,14 @@ namespace SmartCloud.Common.Menus
 {
     public interface IMenuAppService : ICrudAppService<MenuDto, Guid>
     {
+        /// <summary>
+        /// 调整菜单到新的上级菜单
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="parentId">上级菜单Id</param>
+        /// <returns></returns>
+        Task AdjutAsync(Guid id, string parentId);
+
         Task<List<MenuDto>> GetListAsync();
     }
 }
--- src/SmartCloud.Common/SmartCloud.Common.Application/Menus/MenuAppService.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Application/Menus/MenuAppService.cs
@@ -1,21 +1,52 @@
-﻿using Volo.Abp.Application.Services;
-using Volo.Abp.Domain.Repositories;
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
 
 namespace SmartCloud.Common.Menus
 {
     public class MenuAppService : CrudAppService<Menu, MenuDto, Guid>, IMenuAppService
     {
         private readonly IMenuRepository _repository;
+        private readonly MenuManager _manager;
 
-        public MenuAppService(IMenuRepository repository) : base(repository)
+        public MenuAppService(
+            IMenuRepository repository,
+            MenuManager manager
+        ) : base(repository)
         {
             _repository = repository;
+            _manager = manager;
         }
 
+        /// <summary>
+        /// 调整菜单到新的上级菜单
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="parentId"></param>
+        /// <returns></returns>
+        public async Task AdjutAsync(Guid id, string parentId)
+        {
+            var menu = await _repository.GetAsync(id);
+            var menuParent = await _repository.GetAsync(new Guid(parentId));
+
+            await _manager.AdjustAsync(menu, menuParent);
+        }
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <returns></returns>
         public async Task<List<MenuDto>> GetListAsync()
         {
             var menus = await _repository.GetListAsync(QueryEnum.All, "");
             return ObjectMapper.Map<List<Menu>, List<MenuDto>>(menus);
+        }
+
+        [RemoteService(false)]
+        public override Task<PagedResultDto<MenuDto>> GetListAsync(PagedAndSortedResultRequestDto input)
+        {
+            return base.GetListAsync(input);
         }
     }
 }
