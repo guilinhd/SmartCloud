2e50ddc12451dbdd1cd8568771ea1ac380d902f2
molwhy <molw_smartcloud@hotmail.com>
2022-06-11 15:31:45 +0800

    添加菜单类型枚举

---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain.Shared/Menus/TypeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCloud.Common.Menus
+{
+    public enum TypeEnum
+    {
+        Multiple,   //Mid多窗口
+        Single,     //Mid单窗口
+        Popup       //弹出窗口
+    }
+}
