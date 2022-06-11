3e5ba3b75c08764c8478334e411f4f75b42bdc2d
molwhy <molw_smartcloud@hotmail.com>
2022-06-11 15:45:53 +0800

    添加仓储实现

---
+++ src/SmartCloud.Common/SmartCloud.Common.EntityFrameworkCore/Menus/EfMenuRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartCloud.Common.EntityFrameworkCore;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace SmartCloud.Common.Menus
+{
+    public class EfMenuRepository : EfCoreRepository<CommonDbContext, Menu, Guid>, IMenuRepository
+    {
+        public EfMenuRepository(IDbContextProvider<CommonDbContext> dbContextProvider) : base(dbContextProvider)
+        {
+        }
+
+        public async Task<List<Menu>> GetListAsync(QueryEnum query, string name)
+        {
+            var dbSet = await GetDbSetAsync();
+
+            return query switch
+            {
+                QueryEnum.ParentId => await dbSet.Where(d => d.ParentId == name).ToListAsync(),
+                _ => await dbSet.ToListAsync(),
+            };
+        }
+    }
+}
