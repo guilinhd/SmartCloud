d086456766266169d74d7e75b746c5eba9ef367d
molwhy <molw_smartcloud@hotmail.com>
2022-06-11 21:28:20 +0800

    添加角色角色查询枚举

---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain.Shared/RoleUsers/QueryEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartCloud.Common.RoleUsers
+{
+    public enum QueryEnum
+    {
+        RoleId,
+        All
+    }
+}
