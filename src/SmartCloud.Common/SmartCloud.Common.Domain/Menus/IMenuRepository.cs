7dbcb8db17a6fdd79956c2daebc730e527df5cc3
molwhy <molw_smartcloud@hotmail.com>
2022-06-11 15:39:47 +0800

    添加领域实体、仓储扩展类

---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain/Menus/IMenuRepository.cs
@@ -0,0 +1,9 @@
+﻿using Volo.Abp.Domain.Repositories;
+
+namespace SmartCloud.Common.Menus
+{
+    public interface IMenuRepository : IRepository<Menu, Guid>
+    {
+        Task<List<Menu>> GetListAsync(QueryEnum query, string name);
+    }
+}
---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain/Menus/Menu.cs
@@ -0,0 +1,23 @@
+﻿using Volo.Abp.Domain.Entities;
+
+namespace SmartCloud.Common.Menus
+{
+    public class Menu : AggregateRoot<Guid>
+    {
+        public int Category { get; set; }
+
+        public int No { get; set; }
+
+        public string ParentId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Href { get; set; }
+
+        public string ImageName { get; set; }
+
+        public string Parameter { get; set; }
+
+        public TypeEnum Type { get; set; }
+    }
+}
