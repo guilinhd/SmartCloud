3dce24ff715be7c6bbf7fecd1fac3f78c044072c
molwhy <molw_smartcloud@hotmail.com>
2022-06-11 17:00:52 +0800

    添加role的领域服务，实现删除功能

---
+++ src/SmartCloud.Common/SmartCloud.Common.Domain/Roles/RoleManager.cs
@@ -0,0 +1,30 @@
+﻿
+using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Domain.Services;
+
+namespace SmartCloud.Common.Roles
+{
+    public class RoleManager : DomainService
+    {
+        private readonly IRepository<Role, Guid> _repository;
+
+        public RoleManager(IRepository<Role, Guid> repository)
+        {
+            _repository = repository;
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="role">实体</param>
+        /// <returns></returns>
+        public async Task DeleteAsync(Role role)
+        {
+            //删除rolemenu
+
+            //删除roleuser
+
+            await _repository.DeleteAsync(role);
+        }
+    }
+}
