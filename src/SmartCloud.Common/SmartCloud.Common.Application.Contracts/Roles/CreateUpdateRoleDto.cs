0115114819d3ed145799092c1fb918603f4f73ac
molwhy <molw_smartcloud@hotmail.com>
2022-06-12 09:41:20 +0800

    添加CreateUpdateRoleDto，一次完成角色、角色用户、角色菜单的提交信息

---
+++ src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/Roles/CreateUpdateRoleDto.cs
@@ -0,0 +1,20 @@
+﻿
+using SmartCloud.Common.RoleMenus;
+using SmartCloud.Common.RoleUsers;
+using Volo.Abp.Application.Dtos;
+
+namespace SmartCloud.Common.Roles
+{
+    public class CreateUpdateRoleDto : AuditedEntityDto<Guid>
+    {
+        public string Name { get; set; }
+
+        public string[] UserIds { get; set; }
+
+        public string[] RoleUserIds { get; set; } 
+
+        public string[] MenuIds { get; set; }   
+
+        public string[] RoleMenuIds { get; set; }
+    }
+}
