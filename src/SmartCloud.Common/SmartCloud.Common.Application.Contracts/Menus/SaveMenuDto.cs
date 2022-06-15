f761f52aa9347e098fd716fb72fa21a585b8f3d1
weijhy <weij_smartcloud@hotmail.com>
2022-06-15 20:53:25 +0800

    增加角色用户信息

--- src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/Menus/SaveMenuDto.cs
+++ src/SmartCloud.Common/SmartCloud.Common.Application.Contracts/Menus/SaveMenuDto.cs
@@ -1,9 +1,12 @@
 ﻿using SmartCloud.Common.RoleMenus;
+using SmartCloud.Common.RoleUsers;
 
 namespace SmartCloud.Common.Menus
 {
     public class SaveMenuDto : MenuDto
     {
         public List<RoleMenuDto> RoleMenus { get; set; }
+
+        public List<RoleUserDto> RoleUsers { get; set; }
     }
 }
